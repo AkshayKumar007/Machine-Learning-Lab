0959a5c6f838bcca631e5d95c0311feacd0e63be
infiniteoverflow <aswingopinathan1871@gmail.com>
2021-01-06 16:44:17 +0530

    Added Program2

---
+++ Program 2 Candidate Elimination/README.md
@@ -0,0 +1,35 @@
+```bash
+    $ python prgm2.py
+```
+
+## Output
+
+ The Given Training Dataset 
+
+['Sunny', 'Warm', 'Normal', 'Strong', 'Warm', 'Same', 'Yes']
+['Sunny', 'Warm', 'High', 'Strong', 'Warm', 'Same', 'Yes']
+['Rainy', 'Cold', 'High', 'Strong', 'Warm', 'Change', 'No']
+['Sunny', 'Warm', 'High', 'Strong', 'Cool', 'Change', 'Yes']
+
+ The initial Value of hypothesis: 
+
+ The most specific hypothesis S0 : [0,0,0,0,0,0]
+
+
+ The most general hypothesis G0 : [?,?,?,?,?,?]
+
+
+Candidate Elimination Algorithm Hypothesis Version Space Computation
+
+--------------------------------------------
+For Training Example No : 1 the hypothesis is S1  ['Sunny', 'Warm', 'Normal', 'Strong', 'Warm', 'Same']
+For Training Example No : 1 the hypothesis is G1  ['?', '?', '?', '?', '?', '?']
+--------------------------------------------
+For Training Example No : 2 the hypothesis is S2  ['Sunny', 'Warm', '?', 'Strong', 'Warm', 'Same']
+For Training Example No : 2 the hypothesis is G2  ['?', '?', '?', '?', '?', '?']
+--------------------------------------------
+For Training Example No :3 the hypothesis is S3  ['Sunny', 'Warm', '?', 'Strong', 'Warm', 'Same']
+For Training Example No :3 the hypothesis is G3  [['Sunny', '?', '?', '?', '?', '?'], ['?', 'Warm', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', 'Same']]
+--------------------------------------------
+For Training Example No : 4 the hypothesis is S4  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+For Training Example No : 4,the hypothesis is G4  [['Sunny', '?', '?', '?', '?', '?'], ['?', 'Warm', '?', '?', '?', '?']]
---
+++ Program 2 Candidate Elimination/prgm2.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  6 16:19:47 2021
+
+@author: aswin
+"""
+
+import csv
+a = []
+print("\n The Given Training Dataset \n")
+
+with open('enjoysport.csv','r') as csvFile:
+    reader = csv.reader(csvFile)
+    for row in reader:
+        a.append(row)
+        print(row)
+num_attributes = len(a[0]) - 1
+
+print("\n The initial Value of hypothesis: ")
+S = ['0']*num_attributes
+G = ['?']*num_attributes
+
+print("\n The most specific hypothesis S0 : [0,0,0,0,0,0]\n")
+print("\n The most general hypothesis G0 : [?,?,?,?,?,?]\n")
+
+
+for j in range(0,num_attributes):
+    S[j] = a[0][j];
+    
+
+print("\nCandidate Elimination Algorithm Hypothesis Version Space Computation\n")
+temp = []
+
+for i in range(0,len(a)):
+    print("--------------------------------------------")
+    if a[i][num_attributes] == 'Yes':
+        for j in range(0,num_attributes):
+            if a[i][j] != S[j]:
+                S[j]='?'
+                
+        for j in range(0,num_attributes):
+            for k in range(1,len(temp)):
+                if temp[k][j] != '?' and temp[k][j] != S[j]:
+                    del temp[k]
+                    
+        print("For Training Example No : {0} the hypothesis is S{0} ".format(i+1),S)
+        if(len(temp) == 0):
+            print("For Training Example No : {0} the hypothesis is G{0} ".format(i+1),G)
+        else :
+            print("For Training Example No : {0},the hypothesis is G{0} ".format(i+1),temp)
+            
+    if a[i][num_attributes] == 'No':
+        for j in range(0,num_attributes):
+            if S[j] != a[i][j] and S[j]!='?':
+                G[j] = S[j]
+                temp.append(G)
+                G = ['?']*num_attributes
+                
+        print("For Training Example No :{0} the hypothesis is S{0} ".format(i+1),S)
+        print("For Training Example No :{0} the hypothesis is G{0} ".format(i+1),temp)
+            
+            
+            
+            
+            
+            
+            
+            
