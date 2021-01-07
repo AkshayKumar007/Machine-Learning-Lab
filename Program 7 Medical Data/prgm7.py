7ad6d247788dbe914a63d1833e68e92e006d0384
infiniteoverflow <aswingopinathan1871@gmail.com>
2021-01-07 09:13:25 +0530

    Updated Program 7

--- Program 7 Medical Data/prgm7.py
+++ Program 7 Medical Data/prgm7.py
@@ -1,44 +1,53 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan  6 22:53:35 2021
-
-@author: aswin
-"""
-
-
-import numpy as np
+''' Q.7) Write a program to construct a Bayesian network considering medical data. Use this model to demonstrate the diagnosis of heart patients using 
+         standard Heart Disease Data Set. You can use Java/Python ML library classes/API.'''
+         
+# A Bayesian Network is a type of Graphical Model that that represents a set of variables and their conditional
+# dependencies via a directed acyclic graph (DAG). 
+         
+# pgmpy is a python library for working with Probabilistic Graphical Models. It allows the user to create their own 
+# graphical models and answer inference or map queries over them. pgmpy has implementation of many inference 
+# algorithms like VariableElimination, Belief Propagation etc.
+         
 import pandas as pd
+import numpy as np
 import csv
 from pgmpy.estimators import MaximumLikelihoodEstimator
 from pgmpy.models import BayesianModel
 from pgmpy.inference import VariableElimination
 
 # Read the attributes
-lines = list(csv.reader(open('heart.csv', 'r')));
-attributes = lines[0]
-# Read Cleveland Heart disease data
-heartDisease = pd.read_csv('heart.csv', names = attributes)
-heartDisease = heartDisease.replace('?', np.nan)
-# Display the data
-print('Few examples from the dataset are given below')
+lines = list(csv.reader(open('heart.csv', 'r')))
+attributes = lines[0]     # Headers
+print(attributes)
+
+# Read the Cleveland Heart Disease data
+heartDisease = pd.read_csv('heart.csv', names= attributes)
+heartDisease = heartDisease.replace('?', np.nan)              # Handling missing values
+
+# View the data
+print('Few examples from the dataset are given below- ')
 print(heartDisease.head())
-print('\nAttributes and datatypes')
+print('\nAttributes and data types-')
 print(heartDisease.dtypes)
-# Model Bayesian Network
-model = BayesianModel([ ('age', 'trestbps'), ('age', 'fbs'), ('sex', 'trestbps'),
-                        ('sex', 'trestbps'), ('exang', 'trestbps'), ('trestbps', 'heartdisease'),
-                        ('fbs', 'heartdisease'), ('heartdisease', 'restecg'), ('heartdisease', 'thalach'),
-                        ('heartdisease', 'chol')])
-# Learning CPDs using Maximum Likelihood Estimators
+
+# Model a Bayesian Network
+model = BayesianModel([('age', 'trestbps'), ('age', 'fbs'), ('sex', 'trestbps'),
+('exang', 'trestbps'), ('trestbps', 'heartdisease'),
+('fbs', 'heartdisease'), ('heartdisease', 'restecg'), ('heartdisease', 'thalach'),
+('heartdisease', 'chol')])
+
+# Learning CPD's (Conditional Probability Distribution) using Maximum Likelihood Estimators
 print('\nLearning CPDs using Maximum Likelihood Estimators...')
 model.fit(heartDisease, estimator=MaximumLikelihoodEstimator)
-# Deducing with Bayesian Network
+
+#Deducing with Bayesian Network
 print('\nInferencing with Bayesian Network:')
 HeartDisease_infer = VariableElimination(model)
-print('\n1.Probability of HeartDisease given Age=20')
-q = HeartDisease_infer.query(variables=['heartdisease'], evidence={'age': 40})
-print(q['heartdisease'])
-print('\n2. Probability of HeartDisease given chol (Cholestoral) =100')
-q = HeartDisease_infer.query(variables=['heartdisease'], evidence={'sex': 0, 'chol': 100})
-print(q['heartdisease'])
+
+print('\n1.Probability of HeartDisease given Age = 20') 
+q = HeartDisease_infer.query(variables=['heartdisease'], evidence={'age': 20})
+print(q)
+
+# print('\n2. Probability of HeartDisease given chol (Cholestoral) = 100')
+# q = HeartDisease_infer.query(variables=['heartdisease'], evidence={'sex': 0, 'chol': 100})
+# print(q)
